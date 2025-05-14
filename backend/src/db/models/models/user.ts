c9df1632d68cf9afb68acb40e082663226c64022
12234626 <pjhalpha@inha.edu>
2025-05-14 15:41:34 +0900

    테스트 모델 파일 삭제

--- backend/src/db/models/models/user.ts
+++ backend/src/db/models/models/user.ts
@@ -3,20 +3,35 @@ import {Model, Table, Column, DataType, PrimaryKey, AllowNull} from "sequelize-t
 // 유저 모델
 @Table({tableName: "user_table"})
 class User extends Model {
-  // 유저 아이디
+  // 유저 아이디(학번)
   @PrimaryKey
   @Column({type: DataType.INTEGER.UNSIGNED})
   public id!: number;
 
-  // 유저 비밀번호
-  @AllowNull(false)
-  @Column({type: DataType.STRING(50)})
-  public password?: string;
-  
   // 유저 이름
   @AllowNull(false)
   @Column({type: DataType.STRING(20)})
   public name!: string;
+  
+  // 유저 비밀번호
+  @AllowNull(false)
+  @Column({type: DataType.INTEGER.UNSIGNED})
+  public password!: number;
+  
+  //유저 학년
+  @AllowNull(false)
+  @Column({type: DataType.INTEGER.UNSIGNED})
+  public grade!: number;
+
+  //유저 학과
+  @AllowNull(false)
+  @Column({type: DataType.STRING(50)})
+  public department!: string;
+
+  //유저 전화번호
+  @AllowNull(false)
+  @Column({type: DataType.INTEGER.UNSIGNED})
+  public phoneNum!: number;
 };
 
 export default User;
