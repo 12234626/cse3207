67a25cac639cb994bad0025ead7ac6e12f53378e
knh0301 <kimna6207@naver.com>
2025-05-15 15:06:45 +0900

    fix: index.js Loginpage/Login

--- frontend/src/Loginpage/Login.jsx
+++ frontend/src/Loginpage/Login.jsx
@@ -1,42 +1,43 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
-    const navigate = useNavigate();
-
-    const handleSignUpClick = () => {
-        navigate("/Signp");
-    }
-
   return (
     <div className="screen">
-      <div className="div">
-
-        <div className="text-wrapper-2"onClick={handleSignUpClick}>회원가입</div>
-
-        <div className="view">
-          <div className="overlap-group">
-            <div className="LOGIN" alt="Login" />
-          </div>
+      <div className="phoneScreen">
+        <div className="findPW">비밀번호찾기</div>
+        <div className="joinTheMembership">회원가입</div>
+        <button className="login">LOGIN</button>
+        <div className="SIDbox">
+          {/* <div className="overlap-group"> */}
+          {/* <div className="text-wrapper-3">비밀번호</div> */}
+          <input
+            type="text"
+            className="inputSid"
+            name="name"
+            id="name"
+            autocomplete="on"
+            placeholder="학번"
+          />
+          {/* </div> */}
         </div>
-
-        <div className="overlap-wrapper">
-          <div className="overlap">
-            <div className="text-wrapper-3">비밀번호</div>
-          </div>
+        <div className="PWbox">
+          {/* <div className="overlap-group"> */}
+          {/* <div className="text-wrapper-4">학번</div> */}
+          <input
+            type="password"
+            className="inputPW"
+            name="pw"
+            id="pw"
+            autocomplete="on"
+            placeholder="비밀번호"
+          />
+          {/* </div> */}
         </div>
-
-        <div className="overlap-group-wrapper">
-          <div className="overlap">
-            <div className="text-wrapper-4">학번</div>
-          </div>
-        </div>
-
-        <div className="view-2" />
+        <div className="logo" />
       </div>
     </div>
   );
-};
+}
 
 export default Login;
