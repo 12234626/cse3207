3316b2aa20d57419de9eda0c74e4ae1d48209eae
knh0301 <kimna6207@naver.com>
2025-05-16 14:07:31 +0900

    fix: MainDong (영역, 분야, 모집기간 추가 수정 필요 -> 텍스트 길이에 맞게 div크기 달라지게)

--- frontend/src/MainDong.jsx
+++ frontend/src/MainDong.jsx
@@ -3,66 +3,67 @@ import "./MainDong.css";
 
 function MainDong() {
   return (
-    <div className="div-wrapper">
-      <div className="div">
-        <div className="image" />
+    <div className="screen">
+      <div className="phoneScreen">
+        <div className="topBar" />
 
-        <div className="overlap">
-          <div className="view">
-            <div className="user" />
+        <div className="main">
+          <div className="myPageButton">{/* <div className="user" /> */}</div>
+
+          {/* <div className="overlap-group"> */}
+          <div className="hongBoButton">
+            <button className="hongBoPost">
+              홍보게시판
+              {/* <div className="text-wrapper">홍보 게시판</div> */}
+            </button>
           </div>
 
-          <div className="overlap-group">
-            <div className="overlap-group-wrapper">
-              <div className="overlap-group-2">
-                <div className="text-wrapper">행사 게시판</div>
+          <div className="clubButton">
+            <button className="clubPost">
+              동아리
+              {/* <div className="text-wrapper-2">동아리</div> */}
+            </button>
+          </div>
+
+          <div className="view-2">
+            <div className="view-3">
+              <div className="overlap-group-3">
+                <div className="text-wrapper-3">모집중</div>
               </div>
             </div>
 
-            <div className="overlap-wrapper">
-              <div className="overlap-2">
-                <div className="text-wrapper-2">동아리</div>
+            <div className="view-4">
+              <div className="overlap-3">
+                <div className="text-wrapper-4">분야:</div>
+
+                <div className="text-wrapper-5">연구</div>
               </div>
             </div>
 
-            <div className="view-2">
-              <div className="view-3">
-                <div className="overlap-group-3">
-                  <div className="text-wrapper-3">모집중</div>
-                </div>
+            <div className="view-5">
+              <div className="overlap-4">
+                <div className="text-wrapper-4">영역:</div>
+
+                <div className="text-wrapper-6">중앙동아리</div>
               </div>
+            </div>
 
-              <div className="view-4">
-                <div className="overlap-3">
-                  <div className="text-wrapper-4">분야:</div>
+            <div className="clubList">
+              <div className="club">
+                <div className="clubName">동아리01</div>
 
-                  <div className="text-wrapper-5">연구</div>
-                </div>
-              </div>
+                <div className="shortInfo">한줄소개</div>
 
-              <div className="view-5">
-                <div className="overlap-4">
-                  <div className="text-wrapper-4">영역:</div>
-
-                  <div className="text-wrapper-6">중앙동아리</div>
-                </div>
-              </div>
-
-              <div className="view-6">
-                <div className="overlap-5">
-                  <div className="text-wrapper-7">동아리01</div>
-
-                  <div className="text-wrapper-8">한줄소개</div>
-
-                  <div className="view-7">
-                    <div className="overlap-group-4">
-                      <div className="text-wrapper-9">신청</div>
-                    </div>
-                  </div>
+                <div className="apply">
+                  <button className="applyButton">
+                    신청
+                    {/* <div className="text-wrapper-9">신청</div> */}
+                  </button>
                 </div>
               </div>
             </div>
           </div>
+          {/* </div> */}
         </div>
       </div>
     </div>
