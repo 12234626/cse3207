d245ec5f2d29d48d67990017713d56696b5b53b5
knh0301 <kimna6207@naver.com>
2025-05-17 21:33:45 +0900

    fix: fixMemberInfo.jsx fixMemberInfo.css className 수정

--- frontend/src/FixMemberInfo.jsx
+++ frontend/src/FixMemberInfo.jsx
@@ -4,36 +4,38 @@ import "./FixMemberInfo.css";
 function FixMemberInfo() {
   return (
     <div className="screen">
-      <div className="div">
-        <div className="overlap">
-          <div className="view">
-            <div className="view-2">
-              <div className="image"></div>
-              <div className="view-3">
-                <div className="view-4">
-                  <div className="test-wrapper">이름</div>
-                  <div className="rectangle"></div>
-                </div>
-                <div className="view-5">
-                  <div className="text-wrapper">학과</div>
-                  <div className="rectangle-2"></div>
-                </div>
-                <div className="view-6">
-                  <div className="rectangle-3"></div>
-                  <div className="text-wrapper">전화번호</div>
-                </div>
+      <div className="phoneScreen">
+        <div className="fixInfoMain">
+          {/* <div className="view"> */}
+          <div className="infoBox">
+            <div className="profileImg"></div>
+            <div className="infoBox1">
+              <div className="fixNameBox">
+                <div className="fixText">이름</div>
+                <input type="text" className="fNameInput"></input>
+              </div>
+              <div className="fixMajorBox">
+                <div className="fixText">학과</div>
+                <input type="text" className="fMajorInput"></input>
+              </div>
+              <div className="fixPhoneBox">
+                <input type="text" className="fPhoneInput"></input>
+                <div className="fixText">전화번호</div>
               </div>
             </div>
           </div>
-          <div className="overlap-group-wrapper">
-            <div className="overlap-group">
+          {/* </div> */}
+          <button className="overlap-group-wrapper">
+            수정하기
+            {/* <div className="overlap-group">
+              수정하기
               <div className="text-wrapper-2">수정하기</div>
-            </div>
-          </div>
+            </div> */}
+          </button>
         </div>
-        <div className="view-7">
-          <div className="img"></div>
-          <div className="text-wrapper-3">회원 정보 수정</div>
+        <div className="topBar">
+          <button className="back"></button>
+          <div className="fixMemberInfoText">회원 정보 수정</div>
         </div>
       </div>
     </div>
