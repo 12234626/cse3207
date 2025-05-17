96e2576c6095829d68ac369481351c3e54c682c1
knh0301 <kimna6207@naver.com>
2025-05-17 22:34:56 +0900

    fix: RequestStatus.jsx RequestStatus.css className 수정

--- frontend/src/RequestStatus.jsx
+++ frontend/src/RequestStatus.jsx
@@ -12,34 +12,36 @@ function RequestStatus() {
   return (
     <div className="screen">
       <div className="phoneScreen">
-        <div className="overlap">
-          <div className="view">
-            <div className="element">
-              <div className="overlap-group">
-                <div className="text-wrapper">가입요청한 동아리01</div>
-                <div className="overlap-group-wrapper">
-                  <div className="div">
-                    <div className="text-wrapper-2">가입완료</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="element-2">
-              <div className="overlap-group">
-                <div className="text-wrapper">가입요청한 동아리02</div>
-                <div className="overlap-group-wrapper">
-                  <div className="div">
-                    <div className="text-wrapper-3">대기중</div>
-                  </div>
-                </div>
-              </div>
+        {/* <div className="overlap"> */}
+        <div className="requestStatusMain">
+          <div className="requestedClub1Box">
+            <div className="requestedClub1">
+              <div className="requestedClubName">가입요청한 동아리01</div>
+              {/* <div className="overlap-group-wrapper"> */}
+              <button className="statusBox">
+                가입완료
+                {/* <div className="text-wrapper-2">가입완료</div> */}
+              </button>
+              {/* </div> */}
             </div>
           </div>
-          <div className="view-2">
-            <button className="image" onClick={handleBackClick}></button>
-            <div className="text-wrapper-4">동아리 가입 요청 현황</div>
+          <div className="requestedClub2Box">
+            <div className="requestedClub1">
+              <div className="requestedClubName">가입요청한 동아리02</div>
+              {/* <div className="overlap-group-wrapper"> */}
+              <button className="statusBox">
+                대기중
+                {/* <div className="text-wrapper-3">대기중</div> */}
+              </button>
+              {/* </div> */}
+            </div>
           </div>
         </div>
+        <div className="topBar">
+          <button className="back" onClick={handleBackClick}></button>
+          <div className="requestStatusText">동아리 가입 요청 현황</div>
+        </div>
+        {/* </div> */}
       </div>
     </div>
   );
