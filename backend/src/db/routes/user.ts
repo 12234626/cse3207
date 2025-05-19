55d302f8a78a9dce043f771916241582bb0cde1f
12234626 <pjhalpha@inha.edu>
2025-05-19 18:51:58 +0900

    유저 아이디와 비밀번호가 일치하는지 확인하는 기능 추가

--- backend/src/db/routes/user.ts
+++ backend/src/db/routes/user.ts
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {createUser, getAllUsers, getUserById, updateUser, deleteUser} from "../controllers/user";
+import {createUser, getAllUsers, checkUserPassword, getUserById, updateUser, deleteUser} from "../controllers/user";
 
 // 유저 데이터베이스 라우터
 const user_router = Router();
@@ -9,6 +9,8 @@ user_router
 .get("/", getAllUsers)
 // 아이디로 유저 조회
 .get("/:id", getUserById)
+// 유저 아이디와 비밀번호가 일치하는지 확인
+.put("/login", checkUserPassword)
 // 유저 생성
 .post("/", createUser)
 // 유저 업데이트
