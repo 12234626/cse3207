6331bb13ded4cd83e1eb0c09a67131165e2c217e
12234626 <pjhalpha@inha.edu>
2025-05-19 18:48:53 +0900

    유저가 동아리에 가입 요청 보내기 기능 추가

--- backend/src/db/routes/club_request.ts
+++ backend/src/db/routes/club_request.ts
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {deleteClubRequest, getAllClubRequests, getAllUserRequests, updateClubRequestStatus} from "../controllers/club_reqeust";
+import {deleteClubRequest, getAllClubRequests, createClubRequest, getAllUserRequests, updateClubRequestStatus} from "../controllers/club_reqeust";
 
 
 // 동아리 데이터베이스 라우터
@@ -10,6 +10,8 @@ club_request_router
 .get("/club/:club_id", getAllClubRequests)
 // 유저가 가입 신청한 모든 동아리 조회
 .get("/user/:user_id", getAllUserRequests)
+// 동아리 가입 신청 생성
+.post("/user/:user_id/club/:club_id", createClubRequest)
 // 동아리 가입 신청 상태 업데이트
 .put("/:id", updateClubRequestStatus)
 // 동아리 가입 신청 삭제
