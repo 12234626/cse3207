4c685fe73d9e541939b875a0ab3474e4ee718405
knh0301 <kimna6207@naver.com>
2025-05-21 15:44:51 +0900

    동아리가입요청현황 리스트 불러오기

--- frontend/src/MyPage/RequestStatus.jsx
+++ frontend/src/MyPage/RequestStatus.jsx
@@ -1,41 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./RequestStatus.css";
 
 function RequestStatus() {
   const navigate = useNavigate();
 
+  const [club, setClubs] = useState([]);
+
   const handleBackClick = () => {
     navigate("/MyPage");
   };
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (!user) return;
+
+    const fetchRequestState = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/db/club_request/user/${user.id}`
+        );
+        const data = await response.json();
+        setClubs(data);
+      } catch (error) {
+        console.error("가입신청한한 동아리 불러오기 실패", error);
+      }
+    };
+    fetchRequestState();
+  }, []);
 
   return (
     <div className="screen">
       <div className="phoneScreen">
         {/* <div className="overlap"> */}
         <div className="requestStatusMain">
-          <div className="requestedClub1Box">
-            <div className="requestedClub1">
-              <div className="requestedClubName">가입요청한 동아리01</div>
-              {/* <div className="overlap-group-wrapper"> */}
-              <button className="statusBox">
-                가입완료
-                {/* <div className="text-wrapper-2">가입완료</div> */}
-              </button>
-              {/* </div> */}
+          {setClubs.map((club) => (
+            <div className="requestedClub1Box" key={club.club_id}>
+              <div className="requestedClub1">
+                <div className="requestedClubName">가입요청한 동아리01</div>
+                {/* <div className="overlap-group-wrapper"> */}
+                <button className="statusBox">
+                  가입완료
+                  {/* <div className="text-wrapper-2">가입완료</div> */}
+                </button>
+                {/* </div> */}
+              </div>
             </div>
-          </div>
-          <div className="requestedClub2Box">
-            <div className="requestedClub1">
-              <div className="requestedClubName">가입요청한 동아리02</div>
-              {/* <div className="overlap-group-wrapper"> */}
-              <button className="statusBox">
-                대기중
-                {/* <div className="text-wrapper-3">대기중</div> */}
-              </button>
-              {/* </div> */}
-            </div>
-          </div>
+          ))}
         </div>
         <div className="topBar2">
           <button className="back" onClick={handleBackClick}></button>
