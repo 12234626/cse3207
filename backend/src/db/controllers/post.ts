b892e956100fb1d5a76d9b8bc1f956ff0987bec6
12234626 <pjhalpha@inha.edu>
2025-05-21 15:48:29 +0900

    게시글 데이터베이스 라우터 및 컨트롤러 수정

--- backend/src/db/controllers/post.ts
+++ backend/src/db/controllers/post.ts
@@ -2,8 +2,8 @@ import {Request, Response} from "express";
 
 import {runQueryWithResponse, buildWhereClause} from "../utils/controller";
 
-// 모든 게시글 조회
-function getPosts(req: Request, res: Response) {
+// 게시글 조회
+function getPost(req: Request, res: Response) {
   const {where, replacements} = buildWhereClause(req.query);
   const query = "SELECT * FROM post_table" + where;
 
@@ -12,31 +12,33 @@ function getPosts(req: Request, res: Response) {
 
 // 게시글 생성
 function createPost(req: Request, res: Response) {
-  const { id, type, title, content, club_id } = req.body;
+  const {id, type, title, content, club_id} = req.body;
   const query = "INSERT INTO post_table (id, type, title, content, club_id) VALUES (:id, :type, :title, :content, :club_id)";
+  const replacements = {id, type, title, content, club_id};
 
-  runQueryWithResponse(req, res, query, { id, type, title, content, club_id }, 201);
+  runQueryWithResponse(req, res, query, replacements, 201);
 }
 
 // 게시글 업데이트
 function updatePost(req: Request, res: Response) {
-  const { id } = req.params;
-  const { title, content } = req.body;
+  const {id, title, content} = req.body;
   const query = "UPDATE post_table SET title = :title, content = :content WHERE id = :id";
+  const replacements = {id, title, content};
   
-  runQueryWithResponse(req, res, query, { id, title, content }, 200);
+  runQueryWithResponse(req, res, query, replacements, 200);
 }
 
 // 게시글 삭제
 function deletePost(req: Request, res: Response) {
-  const { id } = req.params;
+  const {id} = req.body;
   const query = "DELETE FROM post_table WHERE id = :id";
+  const replacements = {id};
 
-  runQueryWithResponse(req, res, query, { id }, 200);
+  runQueryWithResponse(req, res, query, replacements, 200);
 }
 
 export {
-  getPosts,
+  getPost,
   createPost,
   updatePost,
   deletePost,
