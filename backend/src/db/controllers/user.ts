059e8309ea7cf26167b6c575fd1b1dd54c919ce8
12234626 <pjhalpha@inha.edu>
2025-05-21 15:39:47 +0900

    유저 데이터베이스 라우터 및 컨트롤러 수정

--- backend/src/db/controllers/user.ts
+++ backend/src/db/controllers/user.ts
@@ -2,57 +2,52 @@ import {Request, Response} from "express";
 
 import {runQueryWithResponse, buildWhereClause} from "../utils/controller";
 
-// 모든 유저 조회
-function getAllUsers(req: Request, res: Response) {
+// 유저 조회
+function getUser(req: Request, res: Response) {
   const {where, replacements} = buildWhereClause(req.query);
   const query = "SELECT * FROM user_table" + where;
 
   runQueryWithResponse(req, res, query, replacements, 200);
 }
 
-// 아이디로 유저 조회
-function getUserById(req: Request, res: Response) {
-  const {id} = req.params;
-  const query = "SELECT * FROM user_table WHERE id = :id";
-
-  runQueryWithResponse(req, res, query, {id}, 200);
-}
-
-// 유저 아이디와 비밀번호가 일치하는지 확인
-function checkUserPassword(req: Request, res: Response) {
-  const {id, password} = req.query;
-  const query = "SELECT * FROM user_table WHERE id = :id AND password = :password";
-
-  runQueryWithResponse(req, res, query, {id, password}, 200);
-}
-
 // 유저 생성
 function createUser(req: Request, res: Response) {
   const {id, name, password, department, phone} = req.body;
   const query = "INSERT INTO user_table (id, name, password, department, phone) VALUES (:id, :name, :password, :department, :phone)";
+  const replacements = {id, name, password, department, phone};
 
-  runQueryWithResponse(req, res, query, {id, name, password, department, phone}, 201);
+  runQueryWithResponse(req, res, query, replacements, 201);
+}
+
+// 유저 아이디와 비밀번호가 일치하는지 확인
+function checkUserPassword(req: Request, res: Response) {
+  const {id, password} = req.body;
+  const query = "SELECT * FROM user_table WHERE id = :id AND password = :password";
+  const replacements = {id, password};
+
+  runQueryWithResponse(req, res, query, replacements, 200);
 }
 
 // 유저 업데이트
 function updateUser(req: Request, res: Response) {
   const {id, name, password, department, phone} = req.body;
   const query = "UPDATE user_table SET name = :name, password = :password, department = :department, phone = :phone WHERE id = :id";
+  const replacements = {id, name, password, department, phone};
 
-  runQueryWithResponse(req, res, query, {id, name, password, department, phone}, 200);
+  runQueryWithResponse(req, res, query, replacements, 200);
 }
 
 // 유저 삭제
 function deleteUser(req: Request, res: Response) {
-  const {id} = req.params;
+  const {id} = req.body;
   const query = "DELETE FROM user_table WHERE id = :id";
+  const replacements = {id};
 
-  runQueryWithResponse(req, res, query, {id}, 200);
+  runQueryWithResponse(req, res, query, replacements, 200);
 }
 
 export {
-  getAllUsers,
-  getUserById,
+  getUser,
   checkUserPassword,
   createUser,
   updateUser,
--- backend/src/db/routes/user.ts
+++ backend/src/db/routes/user.ts
@@ -1,21 +1,19 @@
 import {Router} from "express";
-import {createUser, getAllUsers, checkUserPassword, getUserById, updateUser, deleteUser} from "../controllers/user";
+import {getUser, createUser, checkUserPassword, updateUser, deleteUser} from "../controllers/user";
 
 // 유저 데이터베이스 라우터
 const user_router = Router();
 
 user_router
-// 모든 유저 조회
-.get("/", getAllUsers)
-// 아이디로 유저 조회
-.get("/user/:id", getUserById)
-// 유저 아이디와 비밀번호가 일치하는지 확인
-.get("/login", checkUserPassword)
+// 유저 조회
+.get("/", getUser)
 // 유저 생성
 .post("/", createUser)
+// 유저 아이디와 비밀번호가 일치하는지 확인
+.post("/login", checkUserPassword)
 // 유저 업데이트
 .put("/", updateUser)
 // 유저 삭제
-.delete("/:id", deleteUser);
+.delete("/", deleteUser);
 
 export default user_router;
