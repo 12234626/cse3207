bf62d55d6ea50d3ca6cdc2bd53b1c1ea6470d35c
ether <dmsrud0838@naver.com>
2025-05-22 19:44:18 +0900

    동아리 회원 기능 유저 테이블과 조인 코드 추가

--- backend/src/db/controllers/club_member.ts
+++ backend/src/db/controllers/club_member.ts
@@ -5,7 +5,17 @@ import {buildWhereClause, runQueryWithResponse} from "../utils/controller";
 // 동아리 회원 조회
 function getClubMember(req: Request, res: Response) {
   const {where, replacements} = buildWhereClause(req.query);
-  const query = "SELECT * FROM club_member_table" + where;
+  const query = `
+    SELECT 
+      club_member_table.club_id,
+      user_table.id AS user_id,
+      user_table.name,
+      user_table.student_number,
+      user_table.department
+    FROM club_member_table
+    JOIN user_table ON club_member_table.user_id = user_table.id
+    ${where}
+  `;
   
   runQueryWithResponse(req, res, query, replacements, 200);
 }
