62dd2551ccffc699c189119c9358f344f1415927
ether <dmsrud0838@naver.com>
2025-05-23 17:26:46 +0900

    inrecruitment로 통일 수정

--- backend/src/db/controllers/club.ts
+++ backend/src/db/controllers/club.ts
@@ -20,18 +20,18 @@ function getClubAdmin(req: Request, res: Response) {
 
 // 동아리 생성
 function createClub(req: Request, res: Response) {
-  const {name, type, field, admin, is_recruiting = true, introduction, info} = req.body;
-  const query = "INSERT INTO club_table (name, type, field, admin, is_recruiting, introduction, info) VALUES (:name, :type, :field, :admin, :is_recruiting, :introduction, :info)";
-  const replacements = {name, type, field, admin, is_recruiting, introduction, info};
+  const {name, type, field, admin, inrecruitment = true, introduction, info} = req.body;
+  const query = "INSERT INTO club_table (name, type, field, admin, inrecruitment, introduction, info) VALUES (:name, :type, :field, :admin, :inrecruitment, :introduction, :info)";
+  const replacements = {name, type, field, admin, inrecruitment, introduction, info};
 
   runQueryWithResponse(req, res, query, replacements, 201);
 }
 
 // 동아리 업데이트
 function updateClub(req: Request, res: Response) {
-  const {id, name, type, field, admin, is_recruiting} = req.body;
-  const query = "UPDATE club_table SET name = :name, type = :type, field = :field, admin = :admin, is_recruiting = :is_recruiting WHERE id = :id";
-  const replacements = {id, name, type, field, admin, is_recruiting};
+  const {id, name, type, field, admin, inrecruitment} = req.body;
+  const query = "UPDATE club_table SET name = :name, type = :type, field = :field, admin = :admin,inrecruitment = :inrecruitment WHERE id = :id";
+  const replacements = {id, name, type, field, admin, inrecruitment};
 
   runQueryWithResponse(req, res, query, replacements, 200);
 }
