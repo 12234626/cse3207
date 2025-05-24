824aa058e753beb06b9dd716daeccd05bb6e7b7c
ether <dmsrud0838@naver.com>
2025-05-25 03:29:14 +0900

    라우터에 추가

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,6 +1,6 @@
 import {Router} from "express";
 
-import {login, createClub, updateClubRequest} from "../controllers/api";
+import {login, createClub, updateClubRequest, updateClubWithInfoPost} from "../controllers/api";
 
 const api_router = Router();
 
@@ -10,6 +10,8 @@ api_router
 // 동아리 생성
 .post("/create_club", createClub)
 // 동아리 가입 신청 업데이트
-.put("/update_club_request", updateClubRequest);
+.put("/update_club_request", updateClubRequest)
+// 동아리 + 상세 설명 게시글 동시 수정
+.put("/club_with_info_post", updateClubWithInfoPost);
 
 export default api_router;
